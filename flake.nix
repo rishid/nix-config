c9a60f292d7352001ffdcd9fbc3fd3d51203731a
Rishi Dhupar <rishid@gmail.com>
2024-01-05 14:12:54 -0500

    First attempt at switching to flake config for server

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS systems and tools";
+
+  inputs = {
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # We use the unstable nixpkgs repo for some packages.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/release-23.11";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: {
+
+    nixosConfigurations.server = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [ 
+          ./machines/server.nix
+          ./users/rishi/nixos.nix
+        ];
+    };
+
+  };
+
+}
---
+++ hosts/server/disko-config.nix
@@ -0,0 +1,40 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        device = "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_1TB_S5P2NU0W402220Y";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/tmp" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=200M"
+        ];
+      };
+    };
+  };
+}
