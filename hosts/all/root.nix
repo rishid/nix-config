1d459388636b9890be400519fae3f61d6ecd4081
Rishi Dhupar <rishid@gmail.com>
2024-01-12 13:28:16 -0700

    Delete old files

--- hosts/all/root.nix
+++ hosts/all/root.nix
@@ -2,29 +2,13 @@
 
 let
 
-  inherit (lib) mkIf;
-
-  # public keys from the secrets dir
-  # keys = config.modules.secrets.keys.users.all;
-
   # agenix secrets combined secrets toggle
   secrets = config.age.secrets // { inherit (config.modules.secrets) enable; };
 
 in {
 
   # Disallow modifying users outside of this config
-  users.mutableUsers = false;
-
-  # Configure root user
-  users.users.root = {
-    # shell = pkgs.zsh;
-    # hashedPasswordFile = mkIf (secrets.enable) secrets.password-hash.path;
-    # password = mkIf (!secrets.enable) "root";
-    # openssh.authorizedKeys.keys = keys;
-  };
-
-  # Default shell
-  # programs.zsh.enable = true;
+  users.mutableUsers = false;  
 
   # Allow root to work with git on the /etc/nixos directory
   system.activationScripts.root.text = ''
