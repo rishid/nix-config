6bc6e8190aca2c32bc5ba807fa3c85acc56319bf
Rishi Dhupar <rishid@gmail.com>
2024-01-12 21:01:40 -0500

    change GHA username

--- secrets/keys/default.nix
+++ secrets/keys/default.nix
@@ -5,7 +5,10 @@ rec {
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEXm2SFCDKxeYHRM7SClgUh9S/oZKBYRItGTbgDmY/gY rdhupar@bos-mpl6a 2024-01-05T15:03:49Z"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkgaulO1q8vc/9oBiYDiWKbMuVhbqOEajnR1QOprsh0 rishi@nixos 2024-01-05T19:20:13Z"
   ];
-  users.all = builtins.concatLists [ users.rishi ];
+  users.github = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCnPNUGJwIAsoI2obJy+NwvNgqdt1Cd1mU1pvdtC7nk GHA@nixos-config"
+  ];
+  users.all = builtins.concatLists [ users.rishi users.github ];
 
   systems.server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE64OSHAUT4xG55JCpsKgNfH/G+OXPou3PLEQCkbW1W8";
   systems.all = [ systems.server ];
