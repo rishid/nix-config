17ccd3f34c5ab77e5d0e985b10c1eccdc911dc43
Rishi Dhupar <rishid@gmail.com>
2024-01-18 06:38:34 -0500

    Update network.nix

--- hosts/all/network.nix
+++ hosts/all/network.nix
@@ -18,4 +18,12 @@
 
   };
 
+  boot.kernelModules = [ "tcp_bbr" ];
+  # boot.kernel.sysctl = {
+  #  "net.core.default_qdisc" = "fq";
+  #  "net.ipv4.tcp_congestion_control" = "bbr";
+  # };
+
+  # services.resolved.enable = true;
+
 }
