b641f18bfe7f1268ac2895b20f418fe05dbd42f5
Rishi Dhupar <rishid@gmail.com>
2024-02-14 19:39:15 -0500

    Update helpers.nix

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -1,1 +1,21 @@
 
+mkMediaUser = { name, uid }:
+  {
+    inherit name;
+      value =
+        {
+          inherit name uid shell;
+          isNormalUser = true;
+          isSystemUser = false;
+          extraGroups = groups;
+        }
+        // (
+          if password == null
+          then {
+            initialPassword = "helloworld";
+          }
+          else {
+            hashedPassword = password;
+          }
+        );
+    };
