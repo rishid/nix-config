601624a032434699bfe512605c7cea04fd404071
Rishi Dhupar <rishid@gmail.com>
2024-02-24 16:34:11 -0500

    Update paths.nix

--- hosts/server/paths.nix
+++ hosts/server/paths.nix
@@ -11,3 +11,31 @@
     podcasts = "${media}/Podcasts";
   };
 }
+
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.my.paths;
+  types = lib.types;
+in
+{
+  options.my.paths = lib.mkOption {
+    type = types.attrsOf types.path;
+    description = lib.mdDoc ''
+      Path definitions to use in your own configuration.
+
+      You could use the example like this for example:
+      ```nix
+      {
+        subsonic.defaultMusicFolder = "${config.my.paths.media}/Music";
+      }
+      ```
+
+      These options by themselves are a no-op. You have to use them yourself.
+    '';
+    default = { };
+    example = {
+      media = "/mnt/nas/media";
+      persisted = "/persist";
+    };
+  };
+}
