7be06ee9d357ee119904139884767921644b296d
Rishi Dhupar <rishid@gmail.com>
2024-02-27 12:22:44 -0500

    Cleanup

--- hosts/server/paths.nix
+++ hosts/server/paths.nix
@@ -1,4 +1,4 @@
-{ config, lib, this, ... }:
+{ config, lib, ... }:
 let
 
   cfg = config.paths;
@@ -43,31 +43,3 @@ in {
   };
 
 }
-
-# { lib, pkgs, config, ... }:
-# let
-#   cfg = config.my.paths;
-#   types = lib.types;
-# in
-# {
-#   options.my.paths = lib.mkOption {
-#     type = types.attrsOf types.path;
-#     description = lib.mdDoc ''
-#       Path definitions to use in your own configuration.
-
-#       You could use the example like this for example:
-#       ```nix
-#       {
-#         subsonic.defaultMusicFolder = "${config.my.paths.media}/Music";
-#       }
-#       ```
-
-#       These options by themselves are a no-op. You have to use them yourself.
-#     '';
-#     default = { };
-#     example = {
-#       media = "/mnt/nas/media";
-#       persisted = "/persist";
-#     };
-#   };
-# }
