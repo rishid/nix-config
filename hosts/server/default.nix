3a50b997a931e250cf632e715ad15901c7bc3115
Rishi Dhupar <rishid@gmail.com>
2024-02-27 12:22:14 -0500

    Remove old defines

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -3,10 +3,4 @@
   host = "server"; 
   config.modules.secrets.enable = true;
   
-  applicationsPath = "/mnt/storage/applications";
-  backupPath = "/mnt/storage/backup";
-  documentsPath = "/mnt/storage/documents";
-  mediaPath = "/mnt/storage/media";
-  photosPath = "/mnt/storage/photos";
-
 }
