9f4a9b0286e130a550aacfecba88cbaac6cdd8e4
Rishi Dhupar <rishid@gmail.com>
2024-05-02 09:43:21 -0400

    add two hard drives

--- hosts/server/disko-config.nix
+++ hosts/server/disko-config.nix
@@ -1,7 +1,7 @@
 {
   disko.devices = {
     disk = {
-      vdb = {
+      main = {
         device = "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_1TB_S5P2NU0W402220Y";
         type = "disk";
         content = {
@@ -27,14 +27,43 @@
           };
         };
       };
+
+      disk0 = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/disk0";
+              };
+            };
+          };
+        };
+      };
+
+      disk1 = {
+        device = "/dev/sdb";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/disk1";
+              };
+            };
+          };
+        };
+      };
+
     };
-    # nodev = {
-    #   "/tmp" = {
-    #     fsType = "tmpfs";
-    #     mountOptions = [
-    #       "size=2000M"
-    #     ];
-    #   };
-    # };
   };
 }
